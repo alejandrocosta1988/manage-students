654dbf501ee1944cb0033dfc1fc5528e03450fd5
Alejandro Costa <alejandroesfc@gmail.com>
2022-06-29 09:10:41 -0300

    creates students and get their names

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+
+public class Student {
+	
+	String name;
+
+	public Student(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+}
--- src/StudentTest.java
+++ src/StudentTest.java
@@ -1,4 +1,16 @@
 
-public class StudentTest {
+public class StudentTest extends junit.framework.TestCase {
 
+	public void testCreate() {
+		
+		final String firstStudentName = "Alejandro Costa";
+		final String secondStudentName = "Martin Fowler";
+		
+		Student firstStudent = new Student(firstStudentName);
+		Student secondStudent = new Student(secondStudentName);
+		
+		assertEquals(firstStudentName, firstStudent.getName());
+		assertEquals(secondStudentName, secondStudent.getName());
+		
+	}
 }
