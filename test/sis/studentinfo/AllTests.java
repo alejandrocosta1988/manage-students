db65f519cb64972b2b3f7349f4c6f606c24f17c5
Alejandro Costa <alejandroesfc@gmail.com>
2022-07-22 07:04:58 -0300

    implements performance to be able to track test scores for a given student

--- test/sis/studentinfo/AllTests.java
+++ test/sis/studentinfo/AllTests.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ StudentTest.class, BasicGradingStrategyTest.class, HonorsGradingStrategyTest.class })
+@SuiteClasses({ StudentTest.class, BasicGradingStrategyTest.class, HonorsGradingStrategyTest.class,
+				PerformanceTest.class})
 public class AllTests {
 
 }
