3114bd5f9d1b4387c1abe6a41a32a3a0e97b5edc
Alejandro Costa <alejandroesfc@gmail.com>
2022-08-10 15:21:01 -0300

    measures performance using course and hash map

---
+++ test/sis/courseinfo/testHashCodePerformance.java
@@ -0,0 +1,24 @@
+package sis.courseinfo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+public class testHashCodePerformance extends TestCase {
+
+	public void testHashCodePerformance() {
+		final int count = 10000;
+		long start = System.currentTimeMillis();
+		Map<Course, String> map = new HashMap<>();
+		for (int i = 0; i < count; i++) {
+			Course course = new Course("C" + i, "" + i);
+			map.put(course, "");
+		}
+		long stop = System.currentTimeMillis();
+		long elapsed = stop - start;
+		final long arbitraryThreshold = 200;
+		assertTrue("elapsed time = " + elapsed, elapsed < arbitraryThreshold);
+	}
+	
+}
