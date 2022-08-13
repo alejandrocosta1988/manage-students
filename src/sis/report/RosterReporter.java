986e3ee768e7170c7c7b231819e3a3985cb2c236
Alejandro Costa <alejandroesfc@gmail.com>
2022-08-13 09:14:00 -0300

    writes reports using a writer

--- src/sis/report/RosterReporter.java
+++ src/sis/report/RosterReporter.java
@@ -1,5 +1,8 @@
 package sis.report;
 
+import java.io.IOException;
+import java.io.Writer;
+
 import sis.courseinfo.CourseSession;
 import sis.studentinfo.Student;
 
@@ -9,32 +12,32 @@ public class RosterReporter {
 	static final String ROSTER_REPORT_FOOTER = "Number of students enrolled in this course: ";
 	
 	private CourseSession session;
+	private Writer writer;
 	
 	public RosterReporter(CourseSession session) {
 		this.session = session;
 	}
 	
-	public String getReport() {
-		StringBuilder buffer = new StringBuilder();
-		writeHeader(buffer);
-		writeBody(buffer);
-		writeFooter(buffer);
-		return buffer.toString();
+	void writeReport(Writer writer) throws IOException {
+		this.writer = writer;
+		writeHeader();
+		writeBody();
+		writeFooter();
 	}
 	
-	private void writeHeader(StringBuilder buffer) {
-		buffer.append(ROSTER_REPORT_HEADER);
+	private void writeHeader() throws IOException {
+		writer.write(ROSTER_REPORT_HEADER);
 	}
 	
-	private void writeBody(StringBuilder buffer) {
+	private void writeBody() throws IOException {
 		for (Student student : session.getAllStudents()) {
-			buffer.append(student.getName());
-			buffer.append(Report.NEWLINE);
+			writer.write(student.getName());
+			writer.write(Report.NEWLINE);
 		}
 	}
 	
-	private void writeFooter(StringBuilder buffer) {
-		buffer.append(ROSTER_REPORT_FOOTER + session.getNumberOfStudents() + Report.NEWLINE);
+	private void writeFooter() throws IOException {
+		writer.write(ROSTER_REPORT_FOOTER + session.getNumberOfStudents() + Report.NEWLINE);
 	}
-	
+
 }
--- test/sis/report/RosterReporterTest.java
+++ test/sis/report/RosterReporterTest.java
@@ -2,6 +2,9 @@ package sis.report;
 
 import static sis.report.Report.NEWLINE;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.time.LocalDate;
 
 import junit.framework.TestCase;
@@ -11,14 +14,16 @@ import sis.studentinfo.Student;
 
 public class RosterReporterTest extends TestCase {
 	
-	public void testRosterReport() {
+	public void testRosterReport() throws IOException {
 		
 		CourseSession session = CourseSession.create(new Course("ENGL", "101"), LocalDate.of(2022, 7, 15));
 		session.enroll(new Student("Cal Bins"));
 		session.enroll(new Student("Lisa Barnes"));
 		session.enroll(new Student("Lis Barnes"));
 		
-		String rosterReport = new RosterReporter(session).getReport();
+		Writer writer = new StringWriter();
+		new RosterReporter(session).writeReport(writer);
+		String rosterReport = writer.toString();
 		
 		assertEquals(
 				RosterReporter.ROSTER_REPORT_HEADER +
